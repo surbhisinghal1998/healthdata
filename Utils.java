237ddb5f81cb26ff0f4b813518cafcaaab15cad8
surbhi <58076764+surbhisinghal1998@users.noreply.github.com>
2023-02-16 09:55:41 +0530

    Add files via upload

---
+++ Utils.java
@@ -0,0 +1,6 @@
+    package com.example.healthcare.Uitls;
+
+public class Utils {
+    static public String baseUrl = "https://apps.appsmaventech.com/psych_backend/";
+    static public boolean isFromSetting = false;
+}
